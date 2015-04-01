02362b2aa063f6dde2e4eb5627f5791890f41862
zhao111 <shuli.zhao@eleves.ec-nantes.fr>
2015-04-01 22:41:13 +0200

    Finish welcome.jsx

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var browserify  = require('browserify');
 var reactify    = require('reactify');
 var nodejsx     = require('node-jsx').install();
 var Welcome     = require('./welcome.jsx');
-var App         = require('./client.jsx');
+//var App         = require('./client.jsx');
 var data        = require('./data');
 var importer    = require('./import/html');
 var dbAPI       = new (require("./database"));
@@ -16,48 +16,49 @@ var dbAPI       = new (require("./database"));
 var development = process.env.NODE_ENV !== 'production';
 
 function renderWelcome(req, res, next) {
-  var path = url.parse(req.url).pathname;
-  var initialState = {
-    path: path,
-    books: books
-  };
-  dbAPI.getBooks().then(function(books){
-    initialState.books = books;
-    var welcome = React.createElement(Welcome, {initialState:initialState});
+  // var path = url.parse(req.url).pathname;
+  // var initialState = {
+  //   path: path,
+  //   books: books
+  // };
+  // dbAPI.getBooks().then(function(books){
+  //   initialState.books = books;
+    // var welcome = React.createElement(Welcome, {initialState:initialState});
+    var app = React.createElement(Welcome);
     res.send("<!doctype html>\n" +
-          React.renderToString(welcome)+
-          "<script>initialState = "+JSON.stringify(initialState)+"</script>"
+          React.renderToString(app)//+
+          // "<script>initialState = "+JSON.stringify(initialState)+"</script>"
     );
-  });
-}
+  }//);
+//}
 
-function renderApp(req, res, next) {
-  var path = url.parse(req.url).pathname;
-  var bookId = req.params.bookId;
-  var layerId = req.params.layerId;
-  var initialState = {
-    path: path,
-    layerId: layerId,
-    bookId: bookId,
-    chapters: [],
-    layers: [],
-    parts: []
-  };
-  dbAPI.getChapters(layerId).then(function(chap) {
-    initialState.chapters = chap;
-    return dbAPI.getLayers(bookId);
-  }).then(function(layers){
-    initialState.layers = layers;
-    return dbAPI.getPartsInLayer(layerId);
-  }).then(function(parts){
-    initialState.parts = parts;
-    var app = React.createElement(App, {initialState: initialState});
-    res.send("<!doctype html>\n" + 
-        React.renderToString(app) +
-        "<script>initialState = "+JSON.stringify(initialState)+"</script>"
-    );
-  });
-}
+// function renderApp(req, res, next) {
+//   var path = url.parse(req.url).pathname;
+//   var bookId = req.params.bookId;
+//   var layerId = req.params.layerId;
+//   var initialState = {
+//     path: path,
+//     layerId: layerId,
+//     bookId: bookId,
+//     chapters: [],
+//     layers: [],
+//     parts: []
+//   };
+//   dbAPI.getChapters(layerId).then(function(chap) {
+//     initialState.chapters = chap;
+//     return dbAPI.getLayers(bookId);
+//   }).then(function(layers){
+//     initialState.layers = layers;
+//     return dbAPI.getPartsInLayer(layerId);
+//   }).then(function(parts){
+//     initialState.parts = parts;
+//     var app = React.createElement(App, {initialState: initialState});
+//     res.send("<!doctype html>\n" + 
+//         React.renderToString(app) +
+//         "<script>initialState = "+JSON.stringify(initialState)+"</script>"
+//     );
+//   });
+// }
 
 function dbResponse(params, dbAPIMethod) {
   return function(req, res) {
@@ -107,12 +108,12 @@ var app = express();
 if (development) {
   app.get('/assets/bundle.js', function(req, res) {
       res.writeHead(200, {"Content-Type":"text/javascript"});
-      browserify('./client.jsx', {
-        debug: true,
-      })
-      .transform(reactify)
-      .bundle()
-      .pipe(res);
+      // browserify('./client.jsx', {
+      //   debug: true,
+      // })
+      // .transform(reactify)
+      // .bundle()
+      // .pipe(res);
       browserify('./welcome.jsx', {
         debug: true,
       })
@@ -126,7 +127,7 @@ app
   .use('/assets', express.static(path.join(__dirname, 'assets')))
   .use('/api', api)
   .use('/', renderWelcome)
-  .use('/book/:bookId/:layerId', renderApp)
+  // .use('/book/:bookId/:layerId', renderApp)
   .listen(3000, function() {
     console.log('Point your browser at http://localhost:3000');
   });
--- welcome.jsx
+++ welcome.jsx
@@ -3,19 +3,24 @@
 
 var React       = require('react');
 var superagent  = require('superagent');
-var bootstrap = require('react-bootstrap');
+var bootstrap   = require('react-bootstrap');
 
-var PageHeader = bootstrap.PageHeader,
-    Grid = bootstrap.Grid,
+var Grid = bootstrap.Grid,
     Row = bootstrap.Row,
     Col = bootstrap.Col,
+    Panel = bootstrap.Panel,
     Button = bootstrap.Button,
-    Nav = bootstrap.Navbar;
+    Navbar = bootstrap.Navbar,
+    Nav = bootstrap.Nav,
+    NavItem = bootstrap.NavItem,
+    DropdownButton = bootstrap.DropdownButton,
+    MenuItem = bootstrap.MenuItem;
 
-var Nav = React.createClass({
-	render : function() {
-		return (
-            <Nav fixedTop> 
+var NavWelcome = React.createClass({
+  render : function() {
+    return (
+            <Navbar fixedTop> 
+              <Nav>
               <NavItem eventKey={1} href='/'>eZoomBook</NavItem>
               <NavItem eventKey={2} href='#'>Books</NavItem>
               <DropdownButton eventKey={3} title='Help!'>
@@ -23,23 +28,23 @@ var Nav = React.createClass({
                   <MenuItem eventKey='2'>FAQ</MenuItem>
                   <MenuItem eventKey='3'>Contact us</MenuItem>
               </DropdownButton>
-            </Nav>
+              </Nav>
+            </Navbar>
+      )
+  }
+});
 
-			)
-	}
-})
-
-//List of books
 var Books = React.createClass({
   render : function() {
-    return
-        <div book>
-          {this.props.books.map(function(book){
-              return 
-                    <h3>{book.name}</h3>  
-              })  
-          }      
-        </div>;
+    return (
+      <div>
+        {
+          this.props.books.map(function(book){
+              return <h3 key={book.id}>{book.name}</h3>;
+          })
+        }
+      </div>
+      );
   }
 });
 
@@ -53,35 +58,40 @@ var NewBookBtn = React.createClass({
 var MainGrid = React.createClass({
   render : function() {
     return (
-        <Nav>
-        <Grid>
+      <div>
+        <NavWelcome />
+        <Grid className="booklist">
+
           <Row>
             <Col md={8}><h1>Books</h1></Col>
-            <Col md={4}>{NewBookBtn}</Col>
+            <Col md={4}><NewBookBtn/></Col>
           </Row>
-          <Row className="book-list">
+          <Row>
             <Col md={4}>
               <Books books={this.props.books} />
             </Col>
           </Row>
+
         </Grid>
+      </div>
     );
   }
 });
 
-var Welcome = React.createClass({
+var App = React.createClass({
   getInitialState: function() {
     return this.props.initialState;
   },
+
   render: function() {
-    var contents = <MainGrid books={this.state.books}>;
+    var contents = <MainGrid books={this.state.books}/>;
     return (
       <html>
         <head>
           <link rel="stylesheet" href="/assets/style.css" />
           <script src="/assets/bundle.js" />
           <meta charSet="utf8" />
-          <title>WelcomeToMinizoom</title>
+          <title>HomeMinizoom</title>
         </head>
         {contents}
       </html>
